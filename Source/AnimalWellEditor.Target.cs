19520e95210fb4e83e495b2c580f995d8370d9cb
exiao <eliclark307@gmail.com>
2025-12-17 22:22:52 +0800

    Revert "Merge branch 'main' of https://github.com/Elysiumgo/AnimalWell"
    This reverts commit f652ebf3544aea2aae624ec7129853a12a65cf85, reversing
    changes made to b25a26b1853390c03f298cbe104c7effc8e84204.

--- Source/AnimalWellEditor.Target.cs
+++ Source/AnimalWellEditor.Target.cs
@@ -1,15 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class AnimalWellEditorTarget : TargetRules
 {
-	public AnimalWellEditorTarget(TargetInfo Target) : base(Target)
+	public AnimalWellEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-
-		ExtraModuleNames.AddRange( new string[] { "AnimalWell" } );
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("AnimalWell");
 	}
 }
